299479730a7e5464148851c9861c1dfdc92c9e81
MoonLiy_The Lily <moonliy_o@outlook.com>
2024-09-16 00:44:50 +0800

    完成了购买，添加的方法。

--- Taoshen1/Shouhuoji.java
+++ Taoshen1/Shouhuoji.java
@@ -1,32 +1,36 @@
 import java.util.Scanner;
 
 public class Shouhuoji {
-    private int hang;
-    private int lie;
-    private int shendu;
-    Huowu[][][] huowuList = new Huowu[hang][lie][shendu];
+    private int channel;
+    private int depth;
+    Huowu[][] huowuList = new Huowu[channel][depth];
 
-    void insertingCoins(int x, int y) {
-        int index = 0;
+    void insertingCoins(int x) {
         int returnCoin;
-        if (huowuList[x][y][index] == null) {
-            System.err.println("Empty Channel.Quit.");
+        if (huowuList[x][0] == null) {
+            System.err.println("此通道货物为空。不能购买。");
         } else {
-            Huowu a = huowuList[x][y][index];
+            Huowu a = huowuList[x][0];
             int insertedCoinValue = 0;
+            int tempInsertedValue = 0;
             Scanner sc1 = new Scanner(System.in);
 
             for (int i = 0; i >= a.getPrice(); i++) {
                 System.out.println("投入硬币（输入整数1、2或5）:");
-                insertedCoinValue += sc1.nextInt();
-
-                if (insertedCoinValue >= a.getPrice()) {
-                    System.out.println("投币完成。货物已经购买");
-                    returnCoin = insertedCoinValue - a.getPrice();
-                    System.out.println("找零:" + returnCoin);
-                    huowuList[x][y] = null;
+                tempInsertedValue = sc1.nextInt();
+                if (tempInsertedValue != 1 && tempInsertedValue != 2 && tempInsertedValue != 5) {
+                    System.err.println("不能投入除了1，2，5以外的其它面值。请重新投币");
+                    System.err.println("上次投币币值：" + tempInsertedValue + "，" + "已投入币值：" + insertedCoinValue);
                 } else {
 
+                    if (insertedCoinValue >= a.getPrice()) {
+                        System.out.println("投币完成。货物已经购买");
+                        returnCoin = insertedCoinValue - a.getPrice();
+                        System.out.println("找零:" + returnCoin);
+                        huowuList[x][0] = null;
+                    } else {
+
+                    }
                 }
             }
             sc1.close();
@@ -35,51 +39,67 @@ public class Shouhuoji {
 
     int buySomething() {
         int x;
-        int y;
-        int index = 0;
 
-        System.out.println("Buy which? Input x y:");
+        System.out.println("需要购买哪个货物？请输入通道编号：");
         Scanner sc1 = new Scanner(System.in);
         x = sc1.nextInt();
-        y = sc1.nextInt();
         sc1.close();
-        System.out.println("Buying:" + x + " " + y + " " + "Which named:" + huowuList[x - 1][y - 1][index].getName());
-        insertingCoins(x, y);
+        if (huowuList[x - 1][0] == null) {
+            System.err.println("此处没有货物，请选择另外的货物。");
+            return -1;
+        } else {
+            System.out.println("正在购买：" + x + " " + "名字：" + huowuList[x - 1][0].getName());
+            insertingCoins(x);
+            for(int i = 0 ; i<depth-1 ; i++){
+                if(huowuList[x][i+1] != null){
+                huowuList[x][i] = huowuList[x][i+1];
+                }else{
+                    break;
+                }
+            }
+        }
         return 0;
     }
 
     int addItem() {
         int x;
-        int y;
-        int index = 0;
+        int Num;
+        int emptyCounter = 0;
         Huowu a;
         String huowuName1;
         int huowuPrice1;
         Scanner sc1 = new Scanner(System.in);
 
-        System.out.println("Add to What?Input x y.");
+        System.out.println("需要添加什么货物到哪个通道？请按照“货物名称 通道编号 单价 数量输入”");
+        huowuName1 = sc1.next();
         x = sc1.nextInt();
-        y = sc1.nextInt();
-        System.out.println("Adding to " + x + " " + y);
+        huowuPrice1 = sc1.nextInt();
+        Num = sc1.nextInt();
+        sc1.close();
 
-        for (int i = 0; i < shendu; i++) {
-            if (huowuList[x][y][i] == null) {
-                index = i;
-                break;
-            }else if( i == shendu-1 && huowuList[x][y][i]==null ){
-                System.err.println("Full x,y!");
-                return -1;
-            }else{
+        for (int i = 0; i < depth; i++) {
+            if (huowuList[x][i] == null) {
+                emptyCounter++;
+            } else {
                 continue;
             }
         }
-
+        if (Num > emptyCounter) {
+            System.err.println("当前通道容量不足。");
+            return -1;
+        } else {
+            System.out.println("正在添加到：" + x + "通道");
+            a = new Huowu(huowuName1, huowuPrice1);
+            for(int i = depth-emptyCounter ; i<depth ;i++){
+                huowuList[x][i]=a;
+            }
+            System.out.println("货物添加完毕");
+        }
         return 0;
     }
 
     Shouhuoji(int a, int b, int c) {
-        hang = a;
-        lie = b;
-        shendu = c;
+        channel = a;
+        depth = b;
     }
 }
